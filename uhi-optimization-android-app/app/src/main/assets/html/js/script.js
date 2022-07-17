700c07a99737644dcc1593935b06d6ff1aeaf85b
KP020987@cerner.net <>
2022-07-17 22:37:18 +0530

    Added multi language support and used boot strap

--- uhi-optimization-android-app/app/src/main/assets/html/js/script.js
+++ uhi-optimization-android-app/app/src/main/assets/html/js/script.js
@@ -1,4 +1,4 @@
-let speak_btn = document.querySelector("#click-to-speak1");
+let speak_btn = document.querySelector("#click-to-speak");
 let submit_btn = document.querySelector("#click-to-submit");
 
 speak_btn.addEventListener("click", (e) => {
@@ -19,18 +19,15 @@ speak_btn.addEventListener("click", (e) => {
   }
 });
 
-
-submit_btn.addEventListener("click", (e) => {
-
-
-  e.preventDefault();
+function convert(text) {
   var medicine = "";
   var diagnosed = "";
-  var textValue = document.getElementById("textarea_speak").value
-  var textValue = JSON.stringify(textValue)
   var myHeaders = new Headers();
   myHeaders.append("X-Api-Key", "intelli-search-csh");
   myHeaders.append("Content-Type", "application/json");
+
+  var textValue = JSON.stringify(text);
+
 
   var raw = JSON.stringify({
     "text": textValue
@@ -42,16 +39,29 @@ submit_btn.addEventListener("click", (e) => {
     body: raw,
     redirect: 'follow'
   };
+  // Drug - dosage - Duration - frequency
 
   fetch("https://intelli-search-csh.herokuapp.com/extractmedication", requestOptions)
     .then(response => response.text())
     .then(result => {
-      result = JSON.parse(result)
-      result.medications.map(data => {
-        medicine = medicine + data.drugs + " " + data.dosage + " " + data.duration + " " + data.frequency
-        medicine += "<br/>"
-      })
-      document.getElementById("medicines").innerHTML = medicine;
+      result = JSON.parse(result);
+      // result.Status = "Success";
+      // result.medications = [];
+      // result.medications.push ({"drugs":"Med1", "dosage":"500 mg","duration":"10 days", "frequency":"8 hours"});
+      // result.medications.push ({"drugs":"Med2", "dosage":"500 mg","duration":"10 days", "frequency":"8 hours"});
+      if (result.Status == "Success") {
+        medicine = "<div class=\"row\">";
+
+        result.medications.map(data => {
+          medicine = medicine + "<div class=\"col-xl-3 col-sm-12 col-12\"> <div class=\"card\"><div class=\"card-content\"><div class=\"card-body\"><div class=\"media d-flex\"><div class=\"align-self-center\"> <i class=\"fas fas-blue fa-notes-medical fa-large\"> </i> </div><div class=\"media-body text-right\">";
+          medicine = medicine + "<h3>" + data.drugs + " " + data.dosage + "</h3> <span>" + data.duration + " " + data.frequency + "</span>";
+          medicine = medicine + "</div></div></div></div></div></div>";
+        })
+        medicine += "</div>"
+        document.getElementById("medication_container").innerHTML = medicine;
+      } else {
+        document.getElementById("medication_container").innerHTML = "Error";
+      }
     })
     .catch(error => console.log('error', error));
 
@@ -60,16 +70,64 @@ submit_btn.addEventListener("click", (e) => {
     .then(response => response.text())
     .then(result => {
       result = JSON.parse(result)
-      result.extracted_diseases.map(data => {
-        if(diagnosed == ""){
-          diagnosed = data 
-        }
-        diagnosed = diagnosed + "," + data
+      if (result.Status == "Success") {
 
-      })
-      document.getElementById("diagnosis").innerHTML = diagnosed;
+        diagnosed = "<table class=\"table table-bordered table-hover\"><tbody><tr>";
+        result.extracted_diseases.map(data => {
+          diagnosed = diagnosed + "<td>" + data + "</td>";
+        })
+        diagnosed = diagnosed + "</tr></tbody></table>";
+
+        document.getElementById("diagnosis_container").innerHTML = diagnosed;
+      } else {
+        document.getElementById("diagnosis_container").innerHTML = "Error";
+      }
     })
     .catch(error => console.log('error', error));
+}
+submit_btn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  var textValue = document.getElementById("textarea_speak").value;
+  if (textValue == "") {
+    alert("Please Eneter text");
+    return;
+  }
+
+  document.getElementById("medication_container").innerHTML = "";
+  document.getElementById("diagnosis_container").innerHTML = "";
+
+  let element = document.getElementById("language");
+  let language = element.value;
+
+  if (language != "en") {
+
+    var http = new XMLHttpRequest();
+
+    // Unable to host this online 
+    // During demo we will use our local machine to show the demo
+    var url = 'http://localhost:3000/translate';
+    var params = '';
+    http.open('POST', url, true);
+
+    //Send the proper header information along with the request
+    http.setRequestHeader('Content-type', 'application/json');
+
+    http.onreadystatechange = function () { //Call a function when the state changes.
+      if (http.readyState == 4 && http.status == 200) {
+        convert(http.responseText);
+        // alert(http.responseText);
+      }
+    }
+    var params = {
+      "text": textValue,
+      "from": language,
+      "to": "en"
+    };
+    http.send(JSON.stringify(params));
+  } else {
+    convert(textValue);
+  }
 
 
 });
